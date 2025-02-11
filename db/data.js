7d2e49b3a51e6ba0a62c28b06a177a7caa382996
Giovanni Coronel <elvis.coronel@unmsm.edu.pe>
2025-02-11 03:15:00 -0500

    add db con funcionalidades usando js

---
+++ db/data.js
@@ -0,0 +1,20 @@
+export const products = {
+   guitarras:[ 
+    { id: "guitarra_1", name: "Thompson", price: 300,  imagen:"./img/guitarra_01.jpg" ,description: "Excelente relación calidad-precio, ideal para guitarristas principiantes y de nivel intermedio proporcionandoles un sonido claro." },
+    { id: "guitarra_2", name: "Vai", price: 200, imagen:"./img/guitarra_02.webp" , description: "Con una configuración de pastillas de DiMarzio Evolution, mástil ultradelgado y puente flotante Edge Tremolo." },
+    { id: "guitarra_3", name: "Dale", price: 300,  imagen:"./img/guitarra_03.jpg" ,description: "Esta guitarra combina un diseño clásico y funcionalidad, ofreciendo un sonido equilibrado y una construcción duradera." },
+    { id: "guitarra_4", name: "Borland", price: 100, imagen:"../img/guitarra_04.jpg" ,description: "Creada con un diseño elegante y una construcción robusta, nos ofrece una experiencia de tocabilidad cómoda y un sonido claro." },
+    { id: "guitarra_6", name: "Reed", price: 300,  imagen:"./img/guitarra_05.jpg" ,description: "Reconocida por su diseño elegante y su sonido versátil ,adaptables a diferentes estilos musicales y necesidades." },
+    { id: "guitarra_5", name: "Campbell", price: 300,  imagen:"./img/guitarra_06.jpg" ,description: "Creada con una construcción robusta, diseño clásico y un sonido equilibrado, perfecto para quienes buscan iniciar su camino musical." }
+   ],
+  drums:[
+    { id: "bateria_1", name: "Tama", price: 300,  imagen:"./img/bateria_01.jpg"},
+    { id: "bateria_2", name: "Sonor", price: 200, imagen:"./img/bateria_02.jpg"},
+    { id: "bateria_3", name: "Yamaha", price: 300,  imagen:"./img/bateria_03.jpg"},
+  ],
+  others:[
+    { id: "otros_1", name: "Fender American Pro", price: 300,  imagen:"./img/fender.webp"},
+    { id: "otros_2", name: "Grotian", price: 200, imagen:"./img/piano.jfif"},
+    { id: "otros_3", name: "Yamaha YTS-62", price: 300,  imagen:"./img/saxofon.jpg"},
+  ]
+  };
